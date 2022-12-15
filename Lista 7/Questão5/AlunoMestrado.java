29e233640ad8ae90cd5867cb138951c8c9575fe5
HdanielR <116807584+HdanielR@users.noreply.github.com>
2022-12-15 20:41:12 -0300

    Add files via upload

---
+++ Lista 7/Questão5/AlunoMestrado.java
@@ -0,0 +1,6 @@
+
+public class AlunoMestrado extends AlunoPosGraduacao{
+    public AlunoMestrado(String n, int m, Data d, int s, int escolha, String no, String tp){
+        super(n, m, d, s, escolha, no, tp, 'M');
+    }
+}

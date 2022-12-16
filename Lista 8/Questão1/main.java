5e11bbbac33611149fdb21e3981ad56ddd2b2aa5
HdanielR <116807584+HdanielR@users.noreply.github.com>
2022-12-15 21:42:13 -0300

    Add files via upload

---
+++ Lista 8/Questão1/main.java
@@ -0,0 +1,11 @@
+/* 
+
+public class main {
+    public static void main(String[] args) {
+        Retangulo r = new Retangulo(0, 0, 0, 1, 1, 1, 1, 0);
+        Quadrado q = new Quadrado(r);
+        System.out.println("Retangulo: " + r);
+        System.out.println("Quadrado: " + q);
+    }
+}
+*/
